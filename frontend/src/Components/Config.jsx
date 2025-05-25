c2b8f7b819cf32f3a862415970f0cee24605b7b9
Create_first_repo <gauravchoundiye123@gmail.com>
2025-05-25 22:24:41 +0530

    updated version 1

--- frontend/src/Components/Config.jsx
+++ frontend/src/Components/Config.jsx
@@ -1,5 +1,5 @@
 // src/config.js
-const APP_BASE_URL = window.location.origin.includes('localhost')? 'http://127.0.0.1:5176': window.location.origin; // Automatically gets current origin
+const APP_BASE_URL = window.location.origin.includes('localhost')? 'http://127.0.0.1:5176': "https://spotify-clone-frontend-puce.vercel.app/callback"; // Automatically gets current origin
 
 export const SPOTIFY_CLIENT_ID = '7322e3c6dc2e49749868c347b09f83cf';
 export const SPOTIFY_CLIENT_SECRET = '7cfa66411fe0420f8781f0309f5f97a1'; // ⚠️ only for testing/demo
